ea2b0d0bfde833af141babc83cf5285c06c5dc08
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-02-11 14:44:54 +0200

    Update clear-messages.js

--- clear-messages.js
+++ clear-messages.js
@@ -53,48 +53,5 @@ bot.on('ready', () => {
     }
   });
 });
-bot.on("message", function(message) { // when a message is sent
-    if (message.author.equals(bot.user)) return; // if the message is sent by a bot, ignore
-
-    var mutedrole = message.guild.roles.find("name", "muted");
-
-    if (command == "mute") { // creates the command mute
-        if (!message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
-        var mutedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
-        if (!mutedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
-        if (mutedmember.hasPermission("ADMINISTRATOR")) return message.reply("I cannot mute this member!") // if memebr is an admin
-        var mutereasondelete = 10 + mutedmember.user.id.length //sets the length of the kickreasondelete
-        var mutereason = message.content.substring(mutereasondelete).split(" "); // deletes the first letters until it reaches the reason
-        var mutereason = mutereason.join(" "); // joins the list kickreason into one line
-        if (!mutereason) return message.reply("Please indicate a reason for the mute!") // if no reason
-        mutedmember.addRole(mutedrole) //if reason, kick
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't mute because of : ${error}`)); //if error, display error
-        message.reply(`${mutedmember.user} has been muted by ${message.author} because: ${mutereason}`); // sends a message saying he was kicked
-    }
-
-    if (command == "unmute") { // creates the command unmute
-        if (!message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
-        var unmutedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
-        if (!unmutedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
-        unmutedmember.removeRole(mutedrole) //if reason, kick
-            .catch(error => message.reply(`Sorry ${message.author} I couldn't mute because of : ${error}`)); //if error, display error
-        message.reply(`${unmutedmember.user} has been unmuted by ${message.author}!`); // sends a message saying he was kicked
-    }
-
-    if (command == "kick") { // creates the command kick
-        if (!message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
-        var kickedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
-        if (!kickedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
-        if (!kickedmember.kickable) return message.reply("I cannot kick this member!") // if the member is unkickable
-        var kickreasondelete = 10 + kickedmember.user.id.length //sets the length of the kickreasondelete
-        var kickreason = message.content.substring(kickreasondelete).split(" "); // deletes the first letters until it reaches the reason
-        var kickreason = kickreason.join(" "); // joins the list kickreason into one line
-        if (!kickreason) return message.reply("Please indicate a reason for the kick!") // if no reason
-        kickedmember.kick(kickreason) //if reason, kick
-            .catch(error => message.reply(`Sorry @${message.author} I couldn't kick because of : ${error}`)); //if error, display error
-        message.reply(`${kickedmember.user.username} has been kicked by ${message.author.username} because: ${kickreason}`); // sends a message saying he was kicked
-    }
-
-});
 
 bot.login(process.env.BOT_TOKEN);
