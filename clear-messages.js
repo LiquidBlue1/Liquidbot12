82b646ca81b85ae1919d7e895143284624f8a841
LiquidBlue1 <36345980+LiquidBlue1@users.noreply.github.com>
2018-02-11 14:39:35 +0200

    Update clear-messages.js

--- clear-messages.js
+++ clear-messages.js
@@ -62,6 +62,8 @@ bot.on("message", function(message) { // when a message is sent
     var command = args[0].toLowerCase(); // sets the command to lowercase (making it incase sensitive)
     var mutedrole = message.guild.roles.find("name", "muted");
   
+}
+
     if (command == "mute") { // creates the command mute
         if (!message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
         var mutedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
@@ -85,5 +87,20 @@ bot.on("message", function(message) { // when a message is sent
         message.reply(`${unmutedmember.user} has been unmuted by ${message.author}!`); // sends a message saying he was kicked
     }
 
+    if (command == "kick") { // creates the command kick
+        if (!message.member.roles.some(r=>["bot-admin"].includes(r.name)) ) return message.reply("Sorry, you do not have the permission to do this!"); // if author has no perms
+        var kickedmember = message.mentions.members.first(); // sets the mentioned user to the var kickedmember
+        if (!kickedmember) return message.reply("Please mention a valid member of this server!") // if there is no kickedmmeber var
+        if (!kickedmember.kickable) return message.reply("I cannot kick this member!") // if the member is unkickable
+        var kickreasondelete = 10 + kickedmember.user.id.length //sets the length of the kickreasondelete
+        var kickreason = message.content.substring(kickreasondelete).split(" "); // deletes the first letters until it reaches the reason
+        var kickreason = kickreason.join(" "); // joins the list kickreason into one line
+        if (!kickreason) return message.reply("Please indicate a reason for the kick!") // if no reason
+        kickedmember.kick(kickreason) //if reason, kick
+            .catch(error => message.reply(`Sorry @${message.author} I couldn't kick because of : ${error}`)); //if error, display error
+        message.reply(`${kickedmember.user.username} has been kicked by ${message.author.username} because: ${kickreason}`); // sends a message saying he was kicked
+    }
+
+});
 
 bot.login(process.env.BOT_TOKEN);
